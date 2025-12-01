bd8bdbb7e06622a8f2f272e0f92fa4b3a17f4945
katerinetamayo03 <dayerli@unicauca.edu.co>
2025-12-01 12:47:01 -0500

    Forzar deploy

--- mi_proyecto/views.py
+++ mi_proyecto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
+    # Definimos los enlaces que aparecerán en la página de inicio
     urls = [
         {"path": "/admin/", "name": "Administración Django"},
         {"path": "/productos/", "name": "Lista de productos"},
