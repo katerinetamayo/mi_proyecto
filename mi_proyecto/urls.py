c8e2b1da6b19d482edda84daff17efab1f628315
katerinetamayo03 <dayerli@unicauca.edu.co>
2025-12-01 12:22:08 -0500

    actualizo

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
 
+    path('', home, name='home'),
 ]
+
